46f9845f531a4b776e4ed4e6c35402dfb9e52a02
carsonsx <carsonsx@qq.com>
2016-10-11 12:46:54 +0800

    remove installsuffix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -installsuffix hfs -ldflags "-s -w" -o hfs
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -ldflags "-s -w" -o hfs
 
