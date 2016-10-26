646836186c42ae0547f04878674b6fd2e5557b32
carsonsx <carsonsx@qq.com>
2016-10-26 16:55:33 +0800

    simple build output

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ GIT_DIRTY="$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)"
 GIT_DESCRIBE="$(git describe --tags --always)"
 
 #CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_darwin_amd64
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_linux_amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_linux_amd64
 #CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_windows_amd64.exe
 
