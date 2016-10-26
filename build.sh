061f859fad944c8f0627fcf75b584cda726e53b2
carsonsx <carsonsx@qq.com>
2016-10-26 14:22:09 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit=`git rev-parse --short HEAD`" -o bin/hfs_darwin_amd64
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit=`git rev-parse --short HEAD`" -o bin/hfs_linux_amd64
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit=`git rev-parse --short HEAD`" -o bin/hfs_windows_amd64.exe
+# Get the git commit
+GIT_COMMIT="$(git rev-parse --short HEAD)"
+GIT_DIRTY="$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)"
+GIT_DESCRIBE="$(git describe --tags --always)"
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_darwin_amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_linux_amd64
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -v -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_windows_amd64.exe
 
