1177338e7fa76685207fb57e1ea2d8bcf7ec2aeb
carsonsx <carsonsx@qq.com>
2016-11-30 10:42:33 +0800

    add docker push base on source code

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ echo "git commit: ${GIT_COMMIT}"
 echo "git dirty: ${GIT_DIRTY}"
 echo "git describe: ${GIT_DESCRIBE}"
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_darwin_amd64
+#CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_darwin_amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_linux_amd64
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_windows_amd64.exe
+#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a -ldflags "-s -w -X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' -X main.GitDescribe='${GIT_DESCRIBE}'" -o bin/hfs_windows_amd64.exe
 
