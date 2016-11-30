e172662bca7391d210ea08af737682bf03baa791
carsonsx <carsonsx@qq.com>
2016-11-30 10:42:33 +0800

    add docker push base on source code

--- buildindocker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull golang
-docker run --rm -v "$PWD":/hfs -w /hfs golang ./build.sh
---
+++ publish.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Login docker
+docker login
+
+# pull golang image for build
+docker pull golang
+
+# build in docker
+docker run --rm -v "$PWD":/hfs -w /hfs golang ./build.sh
+
+# copy build result
+\cp -f bin/hfs_linux_amd64 docker/hfs
+
+# Stop and remove image if exists
+docker rm -f hfs
+
+set -e
+
+# fetch version
+cd docker
+VERSION="$(./hfs --getversion)"
+
+# Build image
+docker build -t carsonsx/hfs .
+docker tag carsonsx/hfs carsonsx/hfs:${VERSION}
+
+# Push image
+docker push carsonsx/hfs
+docker push carsonsx/hfs:${VERSION}
+
+# Clean
+docker rmi carsonsx/hfs
+docker rmi carsonsx/hfs:${VERSION}
+rm -f Dockerfile hfs
